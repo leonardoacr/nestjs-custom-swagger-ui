a558841f21bd27e7c0bbffb45a61b0b68a05eeda
Leonardo Rodrigues <leonardoacrg.dev@gmail.com>
2024-02-06 23:04:15 -0300

    feat: instantiate swagger document builder in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerDocumentBuilder } from './swagger/swagger-document-builder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const applicationUrl = 'http://localhost:3000/api';
+  const swaggerUrl = 'api/sw';
+  const swaggerDocumentBuilder = new SwaggerDocumentBuilder(
+    app,
+    applicationUrl,
+    swaggerUrl,
+  );
+  swaggerDocumentBuilder.setupSwagger();
+
   await app.listen(3000);
 }
 bootstrap();
