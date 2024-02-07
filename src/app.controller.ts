fbfb3e8e1864def4db88c22a867eb271479233f6
Leonardo Rodrigues <leonardoacrg.dev@gmail.com>
2024-02-06 23:02:06 -0300

    refactor: add swagger decorators to app main route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @ApiTags('app')
+  @Get('hello')
+  @ApiOperation({
+    summary: 'Return the hello world.',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
