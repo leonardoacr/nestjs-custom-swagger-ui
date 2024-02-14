9ea76ea6df65e553fbafb70a9d3ef7d279a3947d
Leonardo Rodrigues <leonardoacrg.dev@gmail.com>
2024-02-14 20:40:57 -0300

    refactor: update swagger UI class with constants from object

--- src/swagger/swagger-ui.class.ts
+++ src/swagger/swagger-ui.class.ts
@@ -1,24 +1,30 @@
+import { SWAGGER_UI_CONSTANTS } from './constants/swagger-ui.constants';
+
 export class SwaggerUI {
   constructor(private readonly applicationUrl: string) {}
 
-  private customfavIcon: string = `${this.applicationUrl}/wwwroot/swagger/assets/nestjs-logo.png`;
-  private customSiteTitle: string = 'Custom Swagger UI';
+  private customSiteTitle = 'Custom Swagger UI';
+  private faviconFilename = 'nestjs-logo.png';
+  private topbarIconFilename = 'app-logo.png';
+
+  private customfavIcon: string = `${this.applicationUrl}/wwwroot/swagger/assets/${this.faviconFilename}`;
   private customCss: string = `
-      .topbar-wrapper { content:url('${this.applicationUrl}/wwwroot/swagger/assets/app-logo.png'); width:242px; height:auto; }
-      .topbar-wrapper svg { visibility: hidden; }
-      .swagger-ui .topbar { background-color: #222222; }
-      .swagger-ui .opblock.opblock-get { background-color: #E4E4E4; border-color: #DADFE1; }
-      .swagger-ui .opblock.opblock-get .opblock-summary-method { background: #222222; }
-      .swagger-ui .opblock.opblock-post { background-color: #E4E4E4; border-color: #DADFE1; }
-      .swagger-ui .opblock.opblock-post .opblock-summary-method { background: #222222; }
-      .swagger-ui .opblock.opblock-delete { background-color: #E4E4E4; border-color: #DADFE1; }
-      .swagger-ui .opblock.opblock-delete .opblock-summary-method { background: #222222; }
-      .swagger-ui .opblock.opblock-patch { background-color: #E4E4E4; border-color: #DADFE1; }
-      .swagger-ui .opblock.opblock-patch .opblock-summary-method { background: #222222; }
-      .swagger-ui .opblock.opblock-put { background-color: #E4E4E4; border-color: #DADFE1; }
-      .swagger-ui .opblock.opblock-put .opblock-summary-method { background: #222222; }
-      .swagger-ui .btn.authorize { border-color: #222222; color: #222222; }
-      .swagger-ui .btn.authorize svg { fill: #222222; } `;
+  .topbar-wrapper { content:url('${this.applicationUrl}/wwwroot/swagger/assets/${this.topbarIconFilename}'); width:242px; height:auto; }
+  .topbar-wrapper svg { visibility: hidden; }
+  .swagger-ui .topbar { background-color: ${SWAGGER_UI_CONSTANTS.TOPBAR.BACKGROUND_COLOR}; }
+  .swagger-ui .opblock.opblock-get { background-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.GET.BACKGROUND_COLOR}; border-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.GET.BORDER_COLOR}; }
+  .swagger-ui .opblock.opblock-get .opblock-summary-method { background: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.GET.SUMMARY_COLOR}; }
+  .swagger-ui .opblock.opblock-post { background-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.POST.BACKGROUND_COLOR}; border-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.POST.BORDER_COLOR}; }
+  .swagger-ui .opblock.opblock-post .opblock-summary-method { background: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.POST.SUMMARY_COLOR}; }
+  .swagger-ui .opblock.opblock-delete { background-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.DELETE.BACKGROUND_COLOR}; border-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.DELETE.BORDER_COLOR}; }
+  .swagger-ui .opblock.opblock-delete .opblock-summary-method { background: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.DELETE.SUMMARY_COLOR}; }
+  .swagger-ui .opblock.opblock-patch { background-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PATCH.BACKGROUND_COLOR}; border-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PATCH.BORDER_COLOR}; }
+  .swagger-ui .opblock.opblock-patch .opblock-summary-method { background: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PATCH.SUMMARY_COLOR}; }
+  .swagger-ui .opblock.opblock-put { background-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PUT.BACKGROUND_COLOR}; border-color: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PUT.BORDER_COLOR}; }
+  .swagger-ui .opblock.opblock-put .opblock-summary-method { background: ${SWAGGER_UI_CONSTANTS.HTTP_METHODS.PUT.SUMMARY_COLOR}; }
+  .swagger-ui .btn.authorize { border-color: ${SWAGGER_UI_CONSTANTS.AUTHORIZE.BACKGROUND_COLOR}; color: ${SWAGGER_UI_CONSTANTS.AUTHORIZE.BACKGROUND_COLOR}; }
+  .swagger-ui .btn.authorize svg { fill: ${SWAGGER_UI_CONSTANTS.AUTHORIZE.BACKGROUND_COLOR}; } `;
+
   private swaggerOptions = {
     persistAuthorization: true,
   };
